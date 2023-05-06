ddd91970409b3a5aa1b482a626278b8f2048f355
alesbe <aesparzabellver@gmail.com>
2023-05-06 14:22:17 +0200

    added index list on html

--- src/main/java/com/alesbe/practica/controller/MainController.java
+++ src/main/java/com/alesbe/practica/controller/MainController.java
@@ -3,9 +3,9 @@ package com.alesbe.practica.controller;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.alesbe.practica.business.entity.Actor;
 import com.alesbe.practica.business.entity.Movie;
 import com.alesbe.practica.business.service.MovieService;
 import com.alesbe.practica.business.service.impl.MovieServiceImpl;
@@ -16,11 +16,9 @@ public class MainController {
     MovieService movieService = new MovieServiceImpl();
     
     @GetMapping("/")
-    public String index() {
-        List<Actor> actors = this.movieService.getActoresByMovieId(40);
-        for (Actor actor : actors) {
-            System.out.println(actor);
-        }
+    public String index(Model model) {
+        List<Movie> allMovies = this.movieService.getAll();
+        model.addAttribute("movies", allMovies);
         return "index";
     }
 }
